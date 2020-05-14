6d140fbb8ede6388cdab4f5b11b09522a7cc0965
Pierre Anken <pierre.anken@gmail.com>
2020-05-14 20:32:23 +0200

    Corrected XSD + BM done

---
+++ G2_FTP_UC1/G2_FTP_UC1/PropertySchema.xsd.cs
@@ -0,0 +1,90 @@
+namespace G2_FTP_UC1.PropertySchema {
+    using Microsoft.XLANGs.BaseTypes;
+    
+    
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.BizTalk.Schema.Compiler", "3.0.1.0")]
+    [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+    [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
+    [SchemaType(SchemaTypeEnum.Property)]
+    [Schema(@"https://G2_FTP_UC1.PropertySchema",@"MessageType")]
+    [System.SerializableAttribute()]
+    [SchemaRoots(new string[] {@"MessageType"})]
+    public sealed class PropertySchema : Microsoft.XLANGs.BaseTypes.SchemaBase {
+        
+        [System.NonSerializedAttribute()]
+        private static object _rawSchema;
+        
+        [System.NonSerializedAttribute()]
+        private const string _strSchema = @"<?xml version=""1.0"" encoding=""utf-16""?>
+<xs:schema xmlns=""http://G2_FTP_UC1.PropertySchema"" xmlns:b=""http://schemas.microsoft.com/BizTalk/2003"" targetNamespace=""https://G2_FTP_UC1.PropertySchema"" xmlns:xs=""http://www.w3.org/2001/XMLSchema"">
+  <xs:annotation>
+    <xs:appinfo>
+      <b:schemaInfo schema_type=""property"" xmlns:b=""http://schemas.microsoft.com/BizTalk/2003"" />
+    </xs:appinfo>
+  </xs:annotation>
+  <xs:element name=""MessageType"" type=""xs:string"">
+    <xs:annotation>
+      <xs:appinfo>
+        <b:fieldInfo propertyGuid=""baa8bd45-34ef-4542-9f64-a6c69eccf009"" />
+      </xs:appinfo>
+    </xs:annotation>
+  </xs:element>
+</xs:schema>";
+        
+        public PropertySchema() {
+        }
+        
+        public override string XmlContent {
+            get {
+                return _strSchema;
+            }
+        }
+        
+        public override string[] RootNodes {
+            get {
+                string[] _RootElements = new string [1];
+                _RootElements[0] = "MessageType";
+                return _RootElements;
+            }
+        }
+        
+        protected override object RawSchema {
+            get {
+                return _rawSchema;
+            }
+            set {
+                _rawSchema = value;
+            }
+        }
+    }
+    
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.BizTalk.Schema.Compiler", "3.0.1.0")]
+    [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+    [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
+    [System.SerializableAttribute()]
+    [PropertyType(@"MessageType",@"https://G2_FTP_UC1.PropertySchema","string","System.String")]
+    [PropertyGuidAttribute(@"baa8bd45-34ef-4542-9f64-a6c69eccf009")]
+    public sealed class MessageType : Microsoft.XLANGs.BaseTypes.MessageDataPropertyBase {
+        
+        [System.NonSerializedAttribute()]
+        private static System.Xml.XmlQualifiedName _QName = new System.Xml.XmlQualifiedName(@"MessageType", @"https://G2_FTP_UC1.PropertySchema");
+        
+        private static string PropertyValueType {
+            get {
+                throw new System.NotSupportedException();
+            }
+        }
+        
+        public override System.Xml.XmlQualifiedName Name {
+            get {
+                return _QName;
+            }
+        }
+        
+        public override System.Type Type {
+            get {
+                return typeof(string);
+            }
+        }
+    }
+}
